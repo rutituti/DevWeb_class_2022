83f97c66ba13b1cadfed19866488650c1b15ee77
RUTH S <ruthsolisvelasco@outlook.com>
2022-08-26 17:54:22 -0500

    LAB5 - Preguntas resueltas. Se agregaron los resultados de los ejercicios al HTML. Falta resolver ejercicio 6

--- PersonalWebPage/scripts/lab5.js
+++ PersonalWebPage/scripts/lab5.js
@@ -8,59 +8,69 @@ Utiliza document.write para producir la salida
 
 function get_numcubes()
 {
-    let num = prompt("Ingresa un numero: ");
+    let num = prompt("Generar Cubos. Ingresa un numero: ");
     console.log("Numero Ingresado = " + num);
     const array_cubes = [0];
-    document.write('<table>')
-    document.write('<tr> <td>NUMERO</td> <td>CUBO</td>  </tr>');
+    let tabla = '<table>';
+    tabla += '<tr> <td>NUMERO</td> <td>CUBO</td>  </tr>';
     for (let i = 1; i <= num; i++) {
         array_cubes.push(i*i*i);
-        document.write('<tr> <td>'+i+'</td> <td>'+array_cubes[i]+'</td></tr>')
+        tabla += '<tr> <td>'+i+'</td> <td>'+array_cubes[i]+'</td></tr>';
     }
-    document.write('</table>') //TODO Mover tabla a la pregunta correcta
+    
     console.log(array_cubes);
 
+    //Codigo para mostrar en HTML
+    document.getElementById("id_tablacubos").innerHTML = tabla;
  
 }
 
-//get_numcubes();
+
+get_numcubes();
 
 
 /*
 
-2:TODO Agregar resultados en la pag
+2:
 Entrada: Usando un prompt se pide el resultado de la suma de 2 números generados de manera aleatoria. 
 Salida: La página debe indicar si el resultado fue correcto o incorrecto, y el tiempo que tardó el usuario en escribir la respuesta.
 */
 function guess_number()
 {
-    let num1 = Math.floor(Math.random()*10);
-    let num2 = Math.floor(Math.random()*10);
+    let num1 = Math.floor(Math.random()*11);
+    let num2 = Math.floor(Math.random()*11);
 
     let suma = num1 + num2;
+    let concatenar = "Suma: " +num1+" + "+num2+" = "+suma;
 
     const start = Date.now();
-    let num_us = prompt("Trata de adiviar el resultado de la SUMA de 2 numeros (0 - 100): ");
+    let num_us = prompt("Trata de adiviar el resultado de la SUMA de 2 numeros (0 - 10): ");
     const stop = Date.now();
     let time_s = (stop-start)/1000
 
     if(suma === num_us)
     {
         console.log("CORRECTO!!");
+        concatenar += '<p> CORRECTO!! </p>';
     }else
     {
         console.log("FALLASTE...");
+        concatenar += '<p> FALLASTE... </p>';
     }
     console.log(num1+" + "+num2+" = "+suma);
-
     console.log("Time taken = " + time_s);
+    
+    concatenar += '<p>'+"Tu respuesta = "+ num_us+'</p>'+"Time taken = " + time_s;
+    document.getElementById("id_adivinaSuma").innerHTML = concatenar;
+
+    
 
 }
 
-//guess_number();
+guess_number();
 
 /*
-3:TODO Agregarlo a la pagina
+3:
 Función: contador. 
 Parámetros: Un arreglo de números. 
 Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo.
@@ -89,14 +99,18 @@ function count_numbers()
             cont_zeros++;//Contar zeros
         }        
     }
-
     console.log("Numeros positivos = "+cont_num_p+"\nNumeros negativos = "+cont_num_n+"\nCeros = "+cont_zeros);
+
+    //CODIGO PARA VERLO EN EL HTML
+    let concatenar =  array;
+    concatenar +=  '<p>'+"Numeros positivos = "+cont_num_p+'</p>'+'<p>'+"Numeros negativos = "+cont_num_n+'</p>'+"Ceros = "+cont_zeros;
+    document.getElementById("id_contador").innerHTML = concatenar;
 }
 
-//count_numbers();
+count_numbers();
 
 /*
-4:TODO Mostrar resultado en la pagina
+4:
 Función: promedios. 
 Parámetros: Un arreglo de arreglos de números. 
 Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz.
@@ -109,12 +123,14 @@ function promedio_fila()
     let matrix = Array.from(Array(filas),()=>new Array(columnas));
     
     const array = [];
+    let concatenar;
+    concatenar = '<p>'+"Matriz de numeros ("+filas+","+columnas+")"+'</p>';
     //Llenar matriz
     for (let i = 0; i < filas; i++) {
         for (let j = 0; j < columnas; j++) {
             matrix[i][j] = Math.floor(Math.random()*11);            
         }
-        
+        concatenar += '<p>'+matrix[i]+'</p>';
     }
     //Sacar promedios
     const promedios = [];
@@ -132,26 +148,39 @@ function promedio_fila()
 
     console.log(matrix);
     console.log(promedios);
+
+     //CODIGO PARA VERLO EN EL HTML
+    concatenar += '<p>'+"Promedio por fila"+'</p>'+promedios;
+    
+     document.getElementById("id_promedios").innerHTML = concatenar;
+
 }
 
-//promedio_fila();
+promedio_fila();
 
 
 /*
-5:TODO
+5:
 Función: inverso. 
 Parámetros: Un número. 
 Regresa: El número con sus dígitos en orden inverso.
 */
 
-function num_inverted(number)
+function num_inverted()
 {
-    
+    let number = prompt("Invertir Digitos. Ingresa un numero (trata de que sea uno largo): ");
     const numero_inv = number.toString().split("").reverse().join("");
     console.log("Original: "+number+"\nInvertido: "+numero_inv);
+
+    let concatenar = '<p>'+"Numero Original"+'</p>'+number;
+
+    concatenar += '<p>'+"Numero Invertido"+'</p>'+numero_inv;
+    
+    document.getElementById("id_inverso").innerHTML = concatenar;
+
 }
 
-num_inverted(12345);
+num_inverted();
 
 /*
 6:TODO
