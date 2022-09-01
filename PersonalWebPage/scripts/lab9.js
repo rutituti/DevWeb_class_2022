b43c1cfdc158acba84d1ea56727d21612012e0cf
RUTH S <ruthsolisvelasco@outlook.com>
2022-09-01 16:47:45 -0500

    Creando mi primer servidor

--- PersonalWebPage/scripts/lab9.js
+++ PersonalWebPage/scripts/lab9.js
@@ -3,7 +3,7 @@
 
 const filesystem = require ('fs');
 filesystem.writeFileSync('hola.txt','Hola desde node');
-filesystem.writeFile('asincrono.txt','Hola desde node');
+//filesystem.writeFile('asincrono.txt','Hola desde node');
 
 const arreglo =[500,2234,32,213,12,34,45,444,56,7,3,2];
 
@@ -26,3 +26,19 @@ for(let item of arreglo)
         console.log(item)
     },item);
 }
+
+//Crear mi primer servidor
+
+const http = require('http');
+
+const server = http.createServer( (request,response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type','text/html');
+    response.write('<!DOCTYPE html>')
+    response.write("<h1> Hola mundo </h1>");
+    response.end();
+
+});
+
+server.listen(3000); //Numero de puerto
+
