805264e631aee78d3703905eac2873363625cb96
RUTH S <ruthsolisvelasco@outlook.com>
2022-09-19 16:38:36 -0500

    Instalando y configurando body parser

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ console.log("APRENDIENDO EXPRESS");
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false})); //se pone el body parser hasta arriba para que se use en todas rutas
+
 //Middleware
 //Registrar Middlewares con funciones anonimas
 
@@ -16,9 +20,16 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+// El orden de las rutas debe ser siempre de lo mas especificio a lo general
+app.use('/robots/new', (request, response, next) => {
+    response.send('Respuesta de la ruta "/robots/new"'); 
+});
+
 app.use('/robots', (request, response, next) => {
     response.send('Respuesta de la ruta "/robots"'); 
 });
+
+
 
 app.use((request, response, next) => {
     console.log('Otro middleware!');
