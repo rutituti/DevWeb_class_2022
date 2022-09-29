960919a75eb531d71095a92248743005862440a1
RUTH S <ruthsolisvelasco@outlook.com>
2022-09-29 16:02:58 -0500

    Creacion de neustra primera Cookie

--- controllers/utiles.controler.js
+++ controllers/utiles.controler.js
@@ -10,11 +10,13 @@ exports.get_new = (request, response, next) => {
 exports.post_new = (request, response, next) => {
     const articulo = new Utiles(request.body.nombre,request.body.precio, request.body.descripcion, request.body.imagen);
     articulo.save();
+    response.setHeader('Set-Cookie', 'nombre_cookie=chocolate');
     response.redirect('/utiles');
 };
 
 exports.get_root = (request, response, next) => {
-
+    const cookie = request.get('Cookie').split(';')[1].trim().split('=')[1];
+    console.log(cookie);
     response.render(path.join('utiles','list.ejs'),{
         utiles: Utiles.fetchAll(),
     });
