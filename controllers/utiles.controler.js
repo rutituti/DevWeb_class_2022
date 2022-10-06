11fef5358a12dbde7d74de566241a9b01c87c184
RUTH S <ruthsolisvelasco@outlook.com>
2022-10-06 11:31:12 -0500

    Insertar tuplas en tabla debase de datos

--- controllers/utiles.controler.js
+++ controllers/utiles.controler.js
@@ -10,18 +10,26 @@ exports.get_new = (request, response, next) => {
 
 exports.post_new = (request, response, next) => {
     const articulo = new Utiles(request.body.nombre,request.body.precio, request.body.descripcion, request.body.imagen);
-    articulo.save();
-    request.session.ultimo_articulo = articulo.nombre;
-    response.setHeader('Set-Cookie', 'nombre_cookie=chocolate');
-    response.redirect('/utiles');
+    articulo.save().then(() => {
+        request.session.ultimo_articulo = articulo.nombre;
+        request.session.info = "El arcitulo "+articulo.nombre+" fue creado existosamente";
+        response.setHeader('Set-Cookie', 'nombre_cookie=chocolate');
+        response.redirect('/utiles');
+    }).catch( (error) => {
+        console.log(error);
+    });
+            
+   
 };
 
 exports.get_root = (request, response, next) => {
     
+    Utiles.fetchAll().then(([rows,fieldData])=>{
+        response.render(path.join('utiles','list.ejs'),{
+            utiles: rows ,
+            ultimo_articulo: request.session.ultimo_articulo ? request.session.ultimo_articulo : '',
+            info: info,
+        });
 
-    response.render(path.join('utiles','list.ejs'),{
-        utiles: Utiles.fetchAll(),
-        ultimo_articulo: request.session.ultimo_articulo ? request.session.ultimo_articulo : '',
-
-    });
+    }).catch(error => console.log(error));
 };
--- models/utiles.model.js
+++ models/utiles.model.js
@@ -1,24 +1,30 @@
-escolares = [{nombre: "Lapiz de madera",precio:'8', descripcion: 'ápiz sencillo No. 2', imagen: 'https://cdn.shopify.com/s/files/1/1051/3728/products/pencils_tombow_general_writing-5_1024x_c68c8980-793c-45c3-8da0-876e9ae37ced_700x.jpg?v=1571304233'}, 
-             {nombre: "Goma Factis",precio:'12', descripcion: 'Goma suave para borrar superficies amplias',imagen: 'https://th.bing.com/th/id/OIP.027eiEJbUCCiFpXtokM4rQHaHa?pid=ImgDet&rs=1'}];
+
+const db = require('../util/database');
 
 module.exports = class Utiles {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(mi_nombre,miprecio,mi_descrip, mi_imagen) {
+    constructor(mi_nombre,miprecio,mi_descripcion, mi_imagen, mi_tipo_id) {
         this.nombre = mi_nombre;
+        this.descripcion = mi_descripcion ? mi_descripcion : 'Agregue una descripcion';
+        this.imagen = mi_imagen ? mi_imagen : 'https://th.bing.com/th/id/OIP.SphiExhMc_acht6UyJ9XTQHaE8?pid=ImgDet&rs=1';
         this.precio = miprecio;
-        this.descripcion = mi_descrip;
-        this.imagen = mi_imagen;
+        this.tipo_id = mi_tipo_id ? mi_tipo_id : 1; 
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        escolares.push(this);
+
+        return db.execute('INSERT INTO utiles (nombre, descripcion, imagen, precio, id_tipo) VALUES (?, ?, ?, ?, ?)',
+        [this.nombre, this.descripcion, this.imagen, this.precio, this.tipo_id]
+        );
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
-        return escolares;
+        return db.execute('SELECT * FROM utiles'); //codigo asincrono
+       
+    
     }
 
 }
